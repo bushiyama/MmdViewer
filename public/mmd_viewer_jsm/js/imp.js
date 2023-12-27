cf758508a6c9316314d6ab80390ef07ed5a60503
bushiyama <45021814+bushiyama@users.noreply.github.com>
2023-12-27 11:13:08 +0900

    Create imp.js

---
+++ public/mmd_viewer_jsm/js/imp.js
@@ -0,0 +1,5 @@
+import * as THREE from 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@r128/build/three.module.js';
+
+import { OrbitControls } from 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@r128/examples/jsm/controls/OrbitControls.js';
+import { MMDLoader } from 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@r128/examples/jsm/loaders/MMDLoader.js';
+import { MMDAnimationHelper } from 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@r128/examples/jsm/animation/MMDAnimationHelper.js';

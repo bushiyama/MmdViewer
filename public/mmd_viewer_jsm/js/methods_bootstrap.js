92c4d8407df41a4a42fab6a05f690c46ff471c80
bushiyama <45021814+bushiyama@users.noreply.github.com>
2023-12-27 11:06:01 +0900

    Update methods_bootstrap.js

--- public/mmd_viewer_jsm/js/methods_bootstrap.js
+++ public/mmd_viewer_jsm/js/methods_bootstrap.js
@@ -1,3 +1,9 @@
+import * as THREE from 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@r128/build/three.module.js';
+
+import { OrbitControls } from 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@r128/examples/jsm/controls/OrbitControls.js';
+import { MMDLoader } from 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@r128/examples/jsm/loaders/MMDLoader.js';
+import { MMDAnimationHelper } from 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@r128/examples/jsm/animation/MMDAnimationHelper.js';
+
 var methods_bootstrap = {
     dialog_open: function(target, backdrop = 'static'){
         $(target).modal({backdrop: backdrop, keyboard: false});
